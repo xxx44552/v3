59fe6d9c0f6cdba3355ba60557400745eb0139a0
artur.kudaev <artur.kudaev@ria.com>
2022-01-27 16:54:16 +0200

    del fake amp

--- server.js
+++ server.js
@@ -19,14 +19,11 @@ server.get('*', async (req, res) => {
 
     await router.push(req.url);
     await router.isReady();
-    const isAmp = router?.currentRoute?._value?.meta?.amp;
-    let filePath = '/build/client/index.html';
-    if (isAmp) {
-        filePath = '/build/client/index.amp.html';
-    }
+    const meta = router?.currentRoute?._value?.meta;
+    console.log(meta);
     const appContent = await renderToString(app);
 
-    fs.readFile(path.join(__dirname, filePath), (err, html) => {
+    fs.readFile(path.join(__dirname, '/build/client/index.html'), (err, html) => {
         if (err) {
             throw err;
         }
